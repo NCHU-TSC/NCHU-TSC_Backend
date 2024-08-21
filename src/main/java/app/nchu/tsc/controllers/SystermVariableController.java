64043870de53145558f1363aecfab46651bfad55
Axisflow <axisflow@smail.nchu.edu.tw>
2024-08-22 00:23:44 +0800

    add systerm variable control

---
+++ src/main/java/app/nchu/tsc/controllers/SystermVariableController.java
@@ -0,0 +1,75 @@
+package app.nchu.tsc.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CookieValue;
+
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
+import com.netflix.graphql.dgs.DgsQuery;
+import com.netflix.graphql.dgs.InputArgument;
+
+import app.nchu.tsc.codegen.types.GQL_SystemVariable;
+import app.nchu.tsc.exceptions.PermissionDeniedException;
+import app.nchu.tsc.exceptions.UnauthenticatedException;
+import app.nchu.tsc.models.Member;
+import app.nchu.tsc.models.SystemVariable;
+import app.nchu.tsc.repositories.SystemVariableRepository;
+import app.nchu.tsc.services.MemberService;
+import app.nchu.tsc.services.SystemVariableService;
+
+@DgsComponent
+public class SystermVariableController {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private SystemVariableRepository systemVariableRepository;
+
+    @Autowired
+    private SystemVariableService systemVariableService;
+
+    @DgsQuery
+    private String systemVariable(@CookieValue UUID member_id, @CookieValue String member_token, @InputArgument String key) {
+        Member operator = memberService.verifyWithToken(member_id, member_token);
+        if(operator == null) throw new UnauthenticatedException();
+
+        if(!operator.getRole().isCanViewSystemVariable()) throw new PermissionDeniedException(member_id);
+
+        return systemVariableService.get(key);
+    }
+
+    @DgsQuery
+    private List<GQL_SystemVariable> systemVariables(@CookieValue UUID member_id, @CookieValue String member_token) {
+        Member operator = memberService.verifyWithToken(member_id, member_token);
+        if(operator == null) throw new UnauthenticatedException();
+
+        if(!operator.getRole().isCanViewSystemVariable()) throw new PermissionDeniedException(member_id);
+
+        List<GQL_SystemVariable> result = new ArrayList<>();
+        List<SystemVariable> systemVariables = systemVariableRepository.findAll();
+
+        for(SystemVariable sv : systemVariables) {
+            result.add(SystemVariableService.toSystemVariable(sv));
+        }
+
+        return result;
+    }
+
+    @DgsMutation
+    private GQL_SystemVariable setSystemVariable(@CookieValue UUID member_id, @CookieValue String member_token, @InputArgument String key, @InputArgument String value) {
+        Member operator = memberService.verifyWithToken(member_id, member_token);
+        if(operator == null) throw new UnauthenticatedException();
+
+        if(!operator.getRole().isCanModifySystemVariable()) throw new PermissionDeniedException(member_id);
+
+        return SystemVariableService.toSystemVariable(systemVariableRepository.save(
+            SystemVariable.builder().key(key).value(value).build()
+        ));
+    }
+    
+}
